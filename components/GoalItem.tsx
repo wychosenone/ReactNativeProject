ac68d0a7ec3ee8f525f207b73d1ae80a108010ab
Aaron_Wang <113875829+wychosenone@users.noreply.github.com>
2025-02-19 18:24:47 -0800

    custom made a pressable button, updated the goalitem and its delete button

--- components/GoalItem.tsx
+++ components/GoalItem.tsx
@@ -1,7 +1,8 @@
-import { Button, StyleSheet, Text, View} from "react-native";
 import React from "react";
+import { Pressable, StyleSheet, Text } from "react-native";
+import { useRouter } from "expo-router";
 import { GoalFromDB } from "@/app";
-import { Link } from "expo-router/build/link/Link";
+import { PressableButton } from "@/components/PressableButton";
 
 interface GoalItemProps {
   goalObj: GoalFromDB;
@@ -9,19 +10,31 @@ interface GoalItemProps {
 }
 
 export default function GoalItem({ goalObj, deleteHandler }: GoalItemProps) {
+  const router = useRouter();
+
   return (
-    <View style={styles.textContainer}>
+    <Pressable
+      style={({ pressed }) => [
+        styles.textContainer,
+        pressed && styles.pressedStyle,
+      ]}
+      onPress={() => {
+        router.push(`/goals/${goalObj.id}`);
+      }}
+      android_ripple={{
+        color: "#ccc",
+        borderless: false,
+        radius: 100,
+      }}
+    >
       <Text style={styles.text}>{goalObj.text}</Text>
-      <Button
-        title="X"
-        onPress={() => {
-          deleteHandler(goalObj.id);
-        }}
+
+      <PressableButton
+        onPress={() => deleteHandler(goalObj.id)}
+        style={styles.deleteBtn}
+        pressedStyle={styles.deleteBtnPressed}
       />
-      <Link asChild href={`/goals/${goalObj.id}?sort="asc"`}>
-        <Button title="info" />
-      </Link>
-    </View>
+    </Pressable>
   );
 }
 
@@ -34,8 +47,19 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginVertical: 10,
   },
+  pressedStyle: {
+    backgroundColor: "#888",
+  },
   text: {
     color: "purple",
     fontSize: 20,
+  },
+  deleteBtn: {
+    backgroundColor: "red",
+    borderRadius: 5,
+    marginLeft: 8,
+  },
+  deleteBtnPressed: {
+    backgroundColor: "#c00",
   },
 });
